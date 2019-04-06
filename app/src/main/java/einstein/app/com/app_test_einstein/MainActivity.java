4e197915afefd686666ec7e44ae5ec4a07f9354b
lorenzo <brainstew927@gmail.com>
2019-04-06 19:02:37 +0200

    initial commit

---
+++ app/src/main/java/einstein/app/com/app_test_einstein/MainActivity.java
@@ -0,0 +1,13 @@
+package einstein.app.com.app_test_einstein;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
